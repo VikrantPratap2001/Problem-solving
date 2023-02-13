b0ab3ffdcd0f11f2ebfd6923a407efebac9a3d7a
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:13:37 +0530

    Create character count.c

---
+++ character count.c
@@ -0,0 +1,18 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    char a[100],ch;
+    int i=0,c=0;
+    printf("Enter the string:");
+    gets(a);
+    printf("Enter the character for count:");
+    scanf("%c",&ch);
+    while(a[i]!='\0')
+    {
+        if(a[i]==ch)
+            c++;
+        i++;
+    }
+    printf("Character count=%d",c);
+}
