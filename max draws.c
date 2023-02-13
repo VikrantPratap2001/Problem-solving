d765b85482bbb70499070cf2e5bd6aff4fcaa592
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:24:54 +0530

    Create max draws.c

---
+++ max draws.c
@@ -0,0 +1,16 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    int t,a[100],i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\n");
+    for(i=0;i<t;i++)
+    {
+        printf("%d\n",a[i]+1);
+    }
+}
