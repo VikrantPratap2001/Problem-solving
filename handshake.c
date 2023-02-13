674c1b7087012471e14fba337cd1e8d2f32c4c33
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:18:08 +0530

    Create handshake.c

---
+++ handshake.c
@@ -0,0 +1,18 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    int n,diag;
+    printf("Enter the strength of the meeting:");
+    scanf("%d",&n);
+    diag=(n*(n-3))/2;
+    if(diag<0)
+    {
+        printf("%d",n-1);
+    }
+    if(diag>=0)
+    {
+        printf("%d",n+diag);
+    }
+
+}
