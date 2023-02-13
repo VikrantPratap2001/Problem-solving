2b06c5a1cab40b836b704a5831be5e4c99d4c2c0
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:06:11 +0530

    Create vowels and constaints.c

---
+++ vowels and constaints.c
@@ -0,0 +1,12 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    char x;
+    printf("Enter an alphabet:");
+    scanf("%c",&x);
+    if((x=='a')||(x=='e')||(x=='i')||(x=='o')||(x=='u')||(x=='A')||(x=='E')||(x=='I')||(x=='O')||(x=='U'))
+        printf("Vowel !!");
+    else
+        printf("Consonant !!");
+}
