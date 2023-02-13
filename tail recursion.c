bc554057abaa558b4eca89eee430d1b28fc0ee79
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 11:11:37 +0530

    Create tail recursion.c

---
+++ tail recursion.c
@@ -0,0 +1,16 @@
+# Problem-solving
+#include<stdio.h>
+int fact(int n)
+{
+    if (n==1 || n==0)
+        return 1;
+    else
+        return n*fact(n-1);
+}
+void main()
+{
+    int N;
+    printf("Enter the value for N:");
+    scanf("%d",&N);
+    printf("%d",fact(N));
+}
