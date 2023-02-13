17d186bd4dbf60fe0b4d163c15957c2a60461892
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:08:17 +0530

    Create armstrong.c

---
+++ armstrong/armstrong.c
@@ -0,0 +1,25 @@
+# Problem-solving
+#include<stdio.h>
+#include<math.h>
+void Arm(int N)
+{
+    int a,c,sum=0;
+    a=N;
+    while(N!=0)
+    {
+        c=N%10;
+        sum=sum+pow(c,3);
+        N=N/10;
+    }
+    if(a==sum)
+        printf("Number is an Armstrong Number !!");
+    else
+        printf("Number is not an Armstrong Number !!");
+}
+void main()
+{
+    int N,a,b,c,sum=0;
+    printf("Enter the number:");
+    scanf("%d",&N);
+    Arm(N);
+}
