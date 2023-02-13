0a35ff89b2180f18097306ad653b50c3c3f3634e
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:24:02 +0530

    Create max height of a triangle.c

---
+++ max height of a triangle.c
@@ -0,0 +1,11 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    int b,area ;
+    float h;
+    printf("Enter the base and area:");
+    scanf("%d %d",&b,&area);
+    h=(2*area)/b;
+    printf("%f",h);
+}
