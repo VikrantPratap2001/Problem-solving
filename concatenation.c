f19d26f06722f2ed67e00201bfaa99dafa67c106
VikrantPratap2001 <117745642+VikrantPratap2001@users.noreply.github.com>
2023-02-13 10:15:51 +0530

    Create concatenation.c

---
+++ concatenation.c
@@ -0,0 +1,23 @@
+# Problem-solving
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[1000],b[1000];
+    int x,j,i,y;
+    printf("Enter first string:");
+    gets(a);
+    printf("Enter second string:");
+    gets(b);
+    x=strlen(a);
+    y=strlen(b);
+    j=0;
+    for(i=x;i<x+y;i++)
+    {
+        a[i]=b[j];
+        j++;
+    }
+    puts(a);
+
+
+}
